2f345cff0d7021b9a761c95c90da48b2bb29c18e
Vadim <korobeinikovv@mail.ru>
2022-01-29 17:53:55 +0300

    Add if

--- proxy/run.js
+++ proxy/run.js
@@ -2,15 +2,20 @@
 
 const target = {
   value: 'hello',
+  value2: 'value2',
 }
 
 const handler = {
   get: function (target, prop, receiver) {
-    console.log('H E L L O !')
+    if (prop === 'value2') {
+      console.log('H E L L O !')
+      return 'this is value 2'
+    }
     return Reflect.get(...arguments)
   },
 }
 
 const proxy = new Proxy(target, handler)
 
-console.log(proxy.value)
+// console.log(proxy.value)
+console.log(proxy.value2)
